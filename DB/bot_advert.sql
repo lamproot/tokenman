7e402b209637056530416b085714fd91e67b1621
quanboxiong <quanboxiong@6clue.com>
2018-05-09 19:53:15 +0800

    更新

--- DB/bot_advert.sql
+++ DB/bot_advert.sql
@@ -70,6 +70,9 @@ ALTER TABLE drafters ADD code VARCHAR(255) NOT NULL DEFAULT "" COMMENT "code";
 ALTER TABLE drafters ADD eth VARCHAR(255) NOT NULL DEFAULT "" COMMENT "eth";
 ALTER TABLE drafters ADD is_del tinyint(2) NOT NULL DEFAULT 0 COMMENT "is_del";
 
+ALTER TABLE group_activity ADD theme_id int(10) NOT NULL DEFAULT 1 COMMENT "主题ID";
+ALTER TABLE group_activity ADD tokenman_url VARCHAR(255) NOT NULL DEFAULT "" COMMENT "tokenman_url";
+ALTER TABLE group_activity ADD model_logo VARCHAR(255) NOT NULL DEFAULT "" COMMENT "model_logo";
 
 ALTER TABLE group_activity ADD step1 VARCHAR(255) NOT NULL DEFAULT "" COMMENT "step1";
 ALTER TABLE group_activity ADD step2 VARCHAR(255) NOT NULL DEFAULT "" COMMENT "step2";
@@ -87,8 +90,43 @@ ALTER TABLE group_activity ADD twitter VARCHAR(255) NOT NULL DEFAULT "" COMMENT 
 ALTER TABLE group_activity ADD facebook VARCHAR(255) NOT NULL DEFAULT "" COMMENT "facebook";
 ALTER TABLE group_activity ADD telegram VARCHAR(255) NOT NULL DEFAULT "" COMMENT "telegram";
 ALTER TABLE group_activity ADD web VARCHAR(255) NOT NULL DEFAULT "" COMMENT "web";
+/*
+ Navicat Premium Data Transfer
 
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50627
+ Source Host           : localhost
+ Source Database       : telegram
 
+ Target Server Type    : MySQL
+ Target Server Version : 50627
+ File Encoding         : utf-8
 
+ Date: 05/09/2018 17:51:32 PM
+*/
 
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
 
+-- ----------------------------
+--  Table structure for `activity_theme`
+-- ----------------------------
+DROP TABLE IF EXISTS `activity_theme`;
+CREATE TABLE `activity_theme` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL COMMENT '主题名称',
+  `bglogo` varchar(250) NOT NULL COMMENT 'code页背景图',
+  `dashboard` varchar(250) NOT NULL COMMENT 'dashboard 背景图',
+  `created_at` int(10) NOT NULL COMMENT '创建时间',
+  `updated_at` int(10) NOT NULL COMMENT '修改时间',
+  `is_del` tinyint(2) NOT NULL DEFAULT '0' COMMENT '是否删除',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `activity_theme`
+-- ----------------------------
+BEGIN;
+INSERT INTO `activity_theme` VALUES ('1', '科技感主题', 'http://18.188.112.65:8090/Uploads/images/command/5af0249d30628.png', 'http://18.188.112.65:8090/Uploads/images/command/5af0249d14ed6.png', '0', '0', '0'), ('2', '糖果色主题', 'http://18.188.112.65:8090/Uploads/images/command/5af0249d344b7.png', 'http://18.188.112.65:8090/Uploads/images/command/5af0249d23fdb.png', '0', '0', '0'), ('3', 'aaaa', 'http://localhost:8890/Uploads/images/activity/5af2b0a508249.png', 'http://localhost:8890/Uploads/images/activity/5af2b0a508958.png', '1525854095', '1525854432', '1'), ('4', '', 'http://localhost:8890/Uploads/images/activity/5af2b0e9ed895.png', 'http://localhost:8890/Uploads/images/activity/5af2b0e9ede4e.png', '1525854441', '1525854441', '0');
+COMMIT;
