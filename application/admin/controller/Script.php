c992a600ea12fe515afe078b0678e5e3054ad6c4
quanboxiong <quanboxiong@6clue.com>
2018-06-08 15:58:15 +0800

    更新

--- application/admin/controller/Script.php
+++ application/admin/controller/Script.php
@@ -98,7 +98,7 @@ class Script extends Backend
             $media  = @$feed['entities']['media'];
             if($media) foreach($media as $img) $photos[] = $img['media_url'];
 
-            if ((time() - strtotime($feed['created_at'])) <= 90 * 60) {
+            if ((time() - strtotime($feed['created_at'])) <= 30 * 60) {
                 $data[] = array('id' => $feed['id'], 'tweet'=>$feed['text'],'date'=>strtotime($feed['created_at']),'img'=>$photos);
             }
         }
