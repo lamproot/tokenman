42030f19d75cd6f0419fdbf11e11bdac4fb551ec
lamproot <304151978@qq.com>
2018-07-26 00:14:17 +0800

    gengxin

---
+++ application/index/lang/en/completeinfo.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'registered successfully,our customer service will contact you immediately' => 'registered successfully,our customer service will contact you immediately',
+    'registered successfully please complete your project information' => 'registered successfully please complete your project information',
+
+    "Nickname" => 'Nickname',
+    "Project name" => 'Project name',
+    "Your project name or your team, company name" => 'Your project name or your team, company name',
+    "Name of tokens" => 'Name of tokens',
+    "Such as BTC" => 'Such as BTC',
+    "Robot ID" => 'Robot ID',
+    "The Robot ID you want" => 'The Robot ID you want',
+    "Telegram" => 'Telegram',
+    "Wechat" => 'Wechat',
+    "For costume service contacts you immediately" => "For costume service contacts you immediately",
+    "Back" => 'Back'
+];
---
+++ application/index/lang/zh-cn/completeinfo.php
@@ -0,0 +1,19 @@
+<?php
+
+
+    return [
+        'registered successfully,our customer service will contact you immediately' => '注册成功，我们的客服会立即与您联系',
+        'registered successfully please complete your project information' => '注册成功，请填写您的项目信息',
+
+        "Nickname" => '用户名',
+        "Project name" => '项目名',
+        "Your project name or your team, company name" => '您的项目名称或公司名称',
+        "Name of tokens" => 'Token名称',
+        "Such as BTC" => '如BTC',
+        "Robot ID" => '机器人ID',
+        "The Robot ID you want" => '您希望的ID',
+        "Telegram" => 'Telegram',
+        "Wechat" => '微信',
+        "For costume service contacts you immediately" => "方便我们的客服经理找到您",
+        "Back" => '返回'
+];
