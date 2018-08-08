3eacfeafa52eb0dce4ce9e2cd7f9b9c6039ff1f2
quanboxiong <quanboxiong@6clue.com>
2018-08-08 17:36:13 +0800

    更新

--- application/extra/site.php
+++ application/extra/site.php
@@ -4,7 +4,7 @@ return array (
   'name' => 'TokenMan',
   'beian' => '',
   'cdnurl' => '',
-  'version' => '1.0.6',
+  'version' => '1.0.7',
   'timezone' => 'Asia/Shanghai',
   'forbiddenip' => '',
   'languages' =>
