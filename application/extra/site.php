9740df2ae169bd80f940f5c0941908555826625d
quanboxiong <quanboxiong@6clue.com>
2018-08-08 16:54:22 +0800

    更新

--- application/extra/site.php
+++ application/extra/site.php
@@ -4,7 +4,7 @@ return array (
   'name' => 'TokenMan',
   'beian' => '',
   'cdnurl' => '',
-  'version' => '1.0.5',
+  'version' => '1.0.6',
   'timezone' => 'Asia/Shanghai',
   'forbiddenip' => '',
   'languages' =>
