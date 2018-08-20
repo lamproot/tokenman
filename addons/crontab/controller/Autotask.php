fc668eea947172585e81200f742991639da87c5e
quanboxiong <quanboxiong@6clue.com>
2018-08-20 20:34:04 +0800

    更新

--- addons/crontab/controller/Autotask.php
+++ addons/crontab/controller/Autotask.php
@@ -122,6 +122,7 @@ class Autotask extends Controller
                 }
                 else if ($crontab['type'] == 'shell')
                 {
+                    echo "333";exit;
                     echo $crontab['type'];exit;
                     echo $crontab['content'] . ' >> ' . $logDir . date("Y-m-d") . '.log 2>&1 &';exit;
                     // 执行Shell
