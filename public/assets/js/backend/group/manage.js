5480c2a670b93748a93a2e2d58a36c6b422d7907
quanboxiong <quanboxiong@6clue.com>
2018-08-31 20:08:59 +0800

    xx

--- public/assets/js/backend/group/manage.js
+++ public/assets/js/backend/group/manage.js
@@ -128,6 +128,7 @@ showToggle:false,
                     dataType: 'json',
                     data:{
                         "rule":"del_msg_warn_content",
+                        "value":"",
                         "data":content,
                         "chat_bot_id":getrow['chat_bot_id'],
                         "chat_id":getrow['chat_id']
